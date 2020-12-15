406da81ef832b6ec119bf53197507b3b5478f23d
Omer Kalkan <omer@manjaro-desk.localdomain>
2020-12-15 19:24:03 +0300

    Added change_letter() feature, done some optimizations

--- game.py
+++ game.py
@@ -19,7 +19,20 @@ class TicTacToe():
                 self.current_winner = True
                 return self.current_winner
 
-        letter == "X" if letter == "O" else letter == "X"
+    #Will return the letter/new letter
+    @staticmethod
+    def change_letter(letter_turn):
+            if letter_turn == x_player:
+                letter_turn = o_player
+            else:
+                letter_turn = x_player
+            return letter_turn
+        
+
+        
+    #Check the table staus, if there is a winner, return current_winner = 1
+    def check_status(self):
+        pass
 
     #Print the table
     def print_board(self):
@@ -40,32 +53,32 @@ class TicTacToe():
         return list(self.check_available())
 
 
-def playthegame(game, o_player, print_game):
+def playthegame(game, print_game):
 
     #Start the game, the initialization screen.
     if print_game:
         game.print_board_nums()
-        print("Ilk oyun baslatiliyor.")
-        game.print_board()
+        print("Game initialization... Remember, X plays first.")
 
-
-    letter = "O"
+    current_letter = game.change_letter("ilk")
     while not game.current_winner:
-        if letter == "O":
-            t.make_move(o_player.get_move(t), o_player.letter)
-            time.sleep(0.5)
+        if True:    
+            game.make_move(current_letter.get_move(t), current_letter.letter)
+            time.sleep(0.3)
             os.system("clear")
-            t.print_board()
-
-
+            current_letter = game.change_letter(current_letter)
+            game.print_board()
+            
+            
 
 
 
 
 if __name__ == '__main__':
     #x_player = players.RandomComputerPlayer('X')
-    o_player = players.HumanPlayer('O')
+    o_player = players.HumanPlayer("O")
+    x_player = players.HumanPlayer("X")
     t = TicTacToe()
-    playthegame(t,o_player,print_game=True)
+    playthegame(t,print_game=True)
 
 
